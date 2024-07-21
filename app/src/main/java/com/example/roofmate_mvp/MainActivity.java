b39d60758ce9b296143107f72dd8d45c038b77a9
heshamje24-meet <108745780+heshamje24-meet@users.noreply.github.com>
2024-07-21 22:11:15 +0300

    fixed first 4 pages
    remind me to tell sarah to fix the signup page

--- app/src/main/java/com/example/roofmate_mvp/MainActivity.java
+++ app/src/main/java/com/example/roofmate_mvp/MainActivity.java
@@ -1,30 +1,20 @@
 package com.example.roofmate_mvp;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-
 import androidx.appcompat.app.AppCompatActivity;
-
-
-import com.google.firebase.auth.FirebaseAuth;
 
 import me.pushy.sdk.Pushy;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-
-
     Button submit_button;
     Button submit_button1;
 
-    FirebaseAuth mAuth;
-
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Pushy.listen(this);
@@ -40,7 +30,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         submit_button1.setVisibility(View.VISIBLE);
         submit_button1.setBackgroundColor(Color.TRANSPARENT);
 
-        mAuth = FirebaseAuth.getInstance();
+        // Set click listeners
+        submit_button.setOnClickListener(this);
+        submit_button1.setOnClickListener(this);
     }
 
     @Override
@@ -48,8 +40,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (v == submit_button) {
             Intent intent = new Intent(this, Signin.class);
             startActivity(intent);
-        }
-        if (v == submit_button1) {
+        } else if (v == submit_button1) {
             Intent intent = new Intent(this, Signup.class);
             startActivity(intent);
         }
