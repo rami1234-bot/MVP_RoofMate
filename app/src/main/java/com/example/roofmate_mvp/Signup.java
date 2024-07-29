860f29d1d778846ea34eaf52519fb072403b26ab
RamiSa24-meet <139688880+RamiSa24-meet@users.noreply.github.com>
2024-07-29 17:07:56 +0300

    sd

--- app/src/main/java/com/example/roofmate_mvp/Signup.java
+++ app/src/main/java/com/example/roofmate_mvp/Signup.java
@@ -129,7 +129,7 @@ public class Signup extends AppCompatActivity {
                     if (task.isSuccessful()) {
                         Toast.makeText(Signup.this, "Sign Up Successful", Toast.LENGTH_SHORT).show();
 
-                        Intent intent = new Intent(Signup.this, verf.class);
+                        Intent intent = new Intent(Signup.this, interests.class);
                         intent.putExtra("user", newUser);
                         intent.putExtra("phoneNumber", phoneNumber);
                         startActivity(intent);
